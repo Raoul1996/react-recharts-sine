e8427f1540c0f1ad76275abb153c4f7168664a7f
Raoul1996 <baobaozhou199606@gmail.com>
2017-09-29 00:10:25 +0800

    制作正弦表生成器

--- app/src/components/app.js
+++ app/src/components/app.js
@@ -3,87 +3,122 @@
  */
 import React, {Component} from 'react'
 import {message, Form, Input, Button, Checkbox} from 'antd'
-import 'antd/lib/style/index.css'
+import {rounding, calcSine} from '../utils'
+import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts'
 // use css modules
 import style from './app.less'
 
 const FormItem = Form.Item
+const {TextArea} = Input
 const formItemLayout = {
   labelCol: {span: 4},
-  wrapperCol: {span: 8}
-}
-const formTailLayout = {
-  labelCol: {span: 4},
-  wrapperCol: {span: 8, offset: 4}
+  wrapperCol: {span: 20}
 }
 
 @Form.create()
 class AppComponent extends Component {
-  state = {
-    checkNick: false
+  constructor(props) {
+    super(props)
+    this.state = {
+      sin: {},
+      data: [], array: []
+    }
+    this.createSineTable = this.createSineTable.bind(this)
   }
-  check = () => {
-    this.props.form.validateFields(
-      (err) => {
-        if (!err) {
-          console.info('success')
-        }
-      }
-    )
+
+  componentDidMount() {
+    this.createSineTable(100, 200, 3)
   }
-  handleChange = (e) => {
+
+  /**
+   * 计算sinTable
+   * @param {number} max 幅值最大值
+   * @param {number} num 计算的点数
+   * @param {number} bit 保留的位数
+   */
+  createSineTable = (max, num, bit) => {
+    const sine = calcSine(num) || []
+    const {xIndex, sineRes} = sine
+    const data = sineRes.map((item, index) => {
+      const res = rounding((item * 0.5 + 0.5) * max, bit, true)
+      return Object.assign({}, {sine: res}, {xIndex: xIndex[index]})
+    }) || []
+    const array = sineRes.map(item => {
+      return rounding((item * 0.5 + 0.5) * max, bit, true)
+    }) || []
     this.setState({
-      checkNick: e.target.checked
-    }, () => {
-      this.props.form.validateFields(['nickname'], {force: true})
+      data, array
+    })
+  }
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        const {max, number, bit} = values
+        this.createSineTable(max, number, bit)
+      }
     })
   }
 
   render() {
     const {getFieldDecorator} = this.props.form
+    const {data, array} = this.state
     return (
       <div className={style.app}>
         <h1 className={style.title}>SPWM波生成器</h1>
         <div className={style.workplace}>
           <div className={style.arguments}>
-            <div>
-              <FormItem {...formItemLayout} label="Name">
-                {getFieldDecorator('username', {
+            <Form onSubmit={this.handleSubmit} className={style.form}>
+              <FormItem {...formItemLayout} label="幅值">
+                {getFieldDecorator('max', {
                   rules: [{
                     required: true,
-                    message: 'Please input your name'
+                    message: '请输入幅值最大值'
                   }]
                 })(
-                  <Input placeholder="Please input your name"/>
+                  <Input placeholder="请输入幅值最大值"/>
                 )}
               </FormItem>
-              <FormItem {...formItemLayout} label="Nickname">
-                {getFieldDecorator('nickname', {
+              <FormItem {...formItemLayout} label="点数">
+                {getFieldDecorator('number', {
                   rules: [{
-                    required: this.state.checkNick,
-                    message: 'Please input your nickname'
+                    required: true,
+                    message: '请输入点数'
                   }]
                 })(
-                  <Input placeholder="Please input your nickname"/>
+                  <Input placeholder="请输入点数"/>
                 )}
               </FormItem>
-              <FormItem {...formTailLayout}>
-                <Checkbox
-                  value={this.state.checkNick}
-                  onChange={this.handleChange}
-                >
-                  Nickname is required
-                </Checkbox>
+              <FormItem {...formItemLayout} label="保留位数">
+                {getFieldDecorator('bit', {
+                  rules: [{
+                    required: true,
+                    message: '请输入保留位数'
+                  }]
+                })(
+                  <Input placeholder="请输入保留位数"/>
+                )}
               </FormItem>
-              <FormItem {...formTailLayout}>
-                <Button type="primary" onClick={this.check}>
-                  Check
+              <FormItem>
+                <Button type="primary" htmlType="submit">
+                  Create
                 </Button>
               </FormItem>
-            </div>
-
+            </Form>
           </div>
-          <div className={style.charts}>在这里显示图表</div>
+          <TextArea autosize={false} style={{resize: 'vertical'}} rows="11"
+                    className={style.textArea} value={array.length > 0 ? array : '在这里显示正弦表数据'}/>
+        </div>
+        <div className={style.charts}>
+          <LineChart className={style.lineChart} width={1100} height={300} data={data}
+                     margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+            <XAxis dataKey="xIndex"/>
+            <YAxis/>
+            <CartesianGrid strokeDasharray="3 3"/>
+            <Tooltip/>
+            <Legend/>
+            <Line type="monotone" dataKey="sine" stroke="#8884d8" activeDot={{r: 8}}/>
+          </LineChart>
         </div>
       </div>
     )
---
+++ app/src/utils.js
@@ -0,0 +1,35 @@
+/**
+ * 四舍五入
+ * @param origin 原来的操作数
+ * @param bit 小数点后保留的位数
+ * @param floor 是否向下取整
+ * @return {number} 返回取整后的数字
+ */
+const rounding = (origin, bit = 3, floor = true) => {
+  if (origin === undefined || bit === undefined) {
+    console.error('err', origin, bit)
+    return
+  }
+  if (floor) {
+    return Math.floor(origin * Math.pow(10, bit)) / Math.pow(10, bit)
+  } else {
+    return Math.ceil(origin * Math.pow(10, bit)) / Math.pow(10, bit)
+  }
+}
+/**
+ * 计算规定个数的正弦值
+ * @param {number} num =需要生成的正弦值的个数
+ * @return {{xIndex: Array, sineRes: Array}} x轴的Index和对应的sin值
+ */
+const calcSine = (num) => {
+  let xIndex = []
+  let sineRes = []
+  for (let i = 0; i < num; i++) {
+    let xIndexItem = 2 * i * Math.PI / num
+    let sineResItem = Math.sin(xIndexItem)
+    xIndex.push(xIndexItem)
+    sineRes.push(sineResItem)
+  }
+  return {xIndex, sineRes}
+}
+export {rounding, calcSine}
